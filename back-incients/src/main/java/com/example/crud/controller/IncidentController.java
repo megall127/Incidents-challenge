efb47c7a805b1d11f8a3a829c3a2cb74266e3869
megall127 <leandro.wilker128@gmail.com>
2025-08-29 18:14:23 -0300

    criando primeira rota para testes /incidents

--- back-incients/src/main/java/com/example/crud/controller/IncidentController.java
+++ back-incients/src/main/java/com/example/crud/controller/IncidentController.java
@@ -1,5 +1,24 @@
 package com.example.crud.controller;
 
+import com.example.crud.model.Incident;
+import com.example.crud.repository.IncidentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class IncidentController {
+
+    @Autowired
+    private IncidentRepository incidentRepository;
+
+    @GetMapping("/incidents")
+    public List<Incident> getAllIncidents() {
+        return incidentRepository.findAll();
+    }
+
+
     
 }
--- back-incients/src/main/java/com/example/crud/repository/IncidentRepository.java
+++ back-incients/src/main/java/com/example/crud/repository/IncidentRepository.java
@@ -1,5 +1,6 @@
 package com.example.crud.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +8,6 @@ import com.example.crud.model.Incident;
 
 @Repository
 public interface IncidentRepository extends JpaRepository<Incident, Long> {
+
+    
 }
